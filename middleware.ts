27e117c2eea1d74c4d43707c0de855f9df98128a
Aikya-004 <aikyaana004@gmail.com>
2025-06-11 17:45:36 +0530

    Implemented clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default clerkMiddleware();
+// Define public routes
+const isPublicRoute = createRouteMatcher([
+
+  '/api/webhooks/clerk', // ✅ public route
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const { userId } = await auth()
+    if (!userId) {
+      return new Response("Unauthorized", { status: 401 })
+    }
+  }
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
